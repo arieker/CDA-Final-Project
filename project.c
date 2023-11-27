df10e9832fd2d28b1bd2d5e469c8a7667761e311
ANTHONY <riekeranthony@gmail.com>
2023-11-27 14:53:03 -0500

    up to sign_extend

--- project.c
+++ project.c
@@ -43,7 +43,6 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
     return 0;
 }
 
-
 /* instruction partition */
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
@@ -57,28 +56,78 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
     *jsec = instruction & 0x3FFFFFF;       // instruction [25-0]
 }
 
-
-
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
+    // Set to default values to avoid potential headache
+    controls->RegDst = 2;    // don't care
+    controls->Jump = 2;      // don't care
+    controls->Branch = 2;    // don't care
+    controls->MemRead = 0;   // disabled
+    controls->MemtoReg = 2;  // don't care
+    controls->ALUOp = 0;     // default value
+    controls->MemWrite = 0;  // disabled
+    controls->ALUSrc = 0;    // default value
+    controls->RegWrite = 0;  // disabled
 
+    // Decode the instruction using the opcode (op)
+    switch (op) {
+        case 0b000000: // R-type instruction
+            controls->RegDst = 1;
+            controls->ALUOp = 0b111;
+            controls->RegWrite = 1;
+            break;
+        case 0b100011: // lw instruction
+            controls->MemRead = 1;
+            controls->MemtoReg = 1;
+            controls->ALUSrc = 1;
+            controls->RegWrite = 1;
+            break;
+        case 0b101011: // sw instruction
+            controls->MemWrite = 1;
+            controls->ALUSrc = 1;
+            break;
+        case 0b000100: // beq instruction
+            controls->Branch = 1;
+            controls->ALUOp = 0b010;
+            break;
+        case 0b000010: // j instruction
+            controls->Jump = 1;
+            break;
+        case 0b000101: // bne instruction
+            controls->Branch = 1;
+            controls->ALUOp = 0b011;
+            break;
+        case 0b001000: // addi instruction
+            controls->ALUOp = 0;
+            controls->RegWrite = 1;
+            controls->ALUSrc = 1;
+            break;
+        //case 0xFFFFFFFF: // Halt condition (replace with the actual value)
+            //return 1;
+        default:
+            // Handle unrecognized opcode
+            break;
+    }
+
+    // No halt condition occurred
+    return 0;
 }
 
 /* Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    *data1 = Reg[r1];
+    *data2 = Reg[r2];
 }
-
 
 /* Sign Extend */
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    *extended_value = (unsigned)((int)(short)(offset & 0xFFFF));
 }
 
 /* ALU operations */
