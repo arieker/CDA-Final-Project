2aed995addd0fb29b471602e36c7b7721466fe7e
KevDas <ke763476@ucf.edu>
2023-11-27 15:06:56 -0500

    added alu_operations

--- project.c
+++ project.c
@@ -143,7 +143,12 @@ void sign_extend(unsigned offset,unsigned *extended_value)
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
+    unsigned operand2 = (ALUSrc == 1) ? extended_value : data2;
 
+    
+    ALU(data1, operand2, ALUOp, ALUresult, Zero);
+
+    return 0;
 }
 
 /* Read / Write Memory */
